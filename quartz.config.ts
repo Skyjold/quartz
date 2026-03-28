81091fbe6d1f74ffe6e580644cc4535c42c8a4ea
a-baha <bahalq@proton.me>
2026-03-28 14:55:36 +0300

    Fix baseUrl in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "a-baha.github.io/quartz",
+    baseUrl: "a-baha.github.io/quarzt"
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
